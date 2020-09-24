537411f42c67dbef7fef6f0e11184da50791e167
Philip-vester <phvesterlund@gmail.com>
2020-09-24 10:33:08 +0200

    Login page styling and funktion alpha

--- FoodApp/FoodApp/App.xaml.cs
+++ FoodApp/FoodApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace FoodApp
         {
             InitializeComponent();
 
-            MainPage = new Login();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
---
+++ FoodApp/FoodApp/LoginPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FoodApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Signin_Clicked(object sender, EventArgs e)
+        {
+            var user = entry_Username.Text;
+
+            if (user == "hej")
+            {
+                //byt ut MainPage mot shoping lista
+                App.Current.MainPage = new MainPage();
+            }
+            else
+            {
+                errorUsername.TextColor = Color.FromHex("#4A4A4A");
+            }
+        }
+    }
+}
